7940d649114a08cb14869c0cb4b5a58834086526
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-08-06 23:57:11 -0400

    Completes the markov.py

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,33 @@
 import random
+import math
 
 # Read in all the words in one go
 with open("input.txt") as f:
     words = f.read()
 
+listOfWords = words.split()
 # TODO: analyze which words can follow other words
 # Your code here
 
 
+startingWords = []
+middleWords = []
+endingWords = []
+
+endingCharacters = ('.?!')
+
+for word in listOfWords:
+    if endingCharacters.count(word[len(word) -1]) or endingCharacters.count(word[len(word) -2]):
+        endingWords.append(word)
+    elif word[0].isupper() or (len(word) > 1 and word[1].isupper()):
+        startingWords.append(word)
+    else:   middleWords.append(word)
+
 # TODO: construct 5 random sentences
 # Your code here
-
+for i in range(5):
+    print(startingWords[random.randint(0,(len(startingWords) -1)) ], end =' ')
+    for i in range (random.randint(0,(len(middleWords) -1))):
+        print(middleWords[random.randint(0,(len(middleWords) -1)) ], end =' ')
+    print(endingWords[random.randint(0,(len(endingWords) -1)) ])
+    print('\n')
